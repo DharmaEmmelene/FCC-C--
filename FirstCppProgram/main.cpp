bf99100701f112c43f61ed09df98010f3c54b659
Dharma <dharmaemmelene@gmail.com>
2022-02-22 15:13:38 +0000

    Breakdown of what each line means

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -2,8 +2,7 @@
 
 int main() // main function --starting point of C++ program (entry point) executed in order
 {
-
   std::cout << "Number 1" << std::endl; // endl --new line
-  std::cout << "Number 2";
-  std::cout << "Number 3";
+  std::cout << "Number 2" << std::endl;
+  std::cout << "Number 3"; // std::cout prints to the console
 }
