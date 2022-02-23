34b309b3601b979155b94ee52ffb4488964acae9
Dharma <dharmaemmelene@gmail.com>
2022-02-23 14:27:27 +0000

    Refactor Data input

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -20,8 +20,9 @@ int main()
 
   std::cout << "Please type your name and age : " << std::endl;
 
-  std::cin >> name;
-  std::cin >> age1;
+  // std::cin >> name;
+  // std::cin >> age1;
+  std::cin >> name >> age1;
 
   std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
 }
