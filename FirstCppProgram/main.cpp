29065776a13a0d8148bca4ac87b08eec68a81cd8
Dharma <dharmaemmelene@gmail.com>
2022-02-23 14:05:43 +0000

    Retrieving data

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -4,15 +4,17 @@
 int main()
 {
   // Printing data
+  /*
   std::cout << "Hello C++20!" << std::endl;
   int age{21};
   std::cout << "Age : " << age << std::endl;
 
   std::cerr << "Error message : Something is wrong" << std::endl;
   std::clog << "Log message : Something happened" << std::endl;
+  */
 
   // Data
-  /*
+
   int age1;
   std::string name;
 
@@ -21,6 +23,5 @@ int main()
   std::cin >> name;
   std::cin >> age1;
 
-  std::cout "Hello " << name << " you are " << age1 << " years old!" << std::endl;
-  */
+  std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
 }
