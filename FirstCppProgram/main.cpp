6af8a681cf033f81d2aa73f6138f835ff0f52662
Dharma <dharmaemmelene@gmail.com>
2022-02-23 16:41:05 +0000

    Data input with spaces

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -3,26 +3,14 @@
 
 int main()
 {
-  // Printing data
-  /*
-  std::cout << "Hello C++20!" << std::endl;
-  int age{21};
-  std::cout << "Age : " << age << std::endl;
+  // Data input with spaces
+  std::string full_name;
+  int age;
 
-  std::cerr << "Error message : Something is wrong" << std::endl;
-  std::clog << "Log message : Something happened" << std::endl;
-  */
+  std::cout << "Please type in your full name and age " << std::endl;
 
-  // Data
+  std::getline(std::cin, full_name);
+  std::cin >> age;
 
-  int age1;
-  std::string name;
-
-  std::cout << "Please type your name and age : " << std::endl;
-
-  // std::cin >> name;
-  // std::cin >> age1;
-  std::cin >> name >> age1;
-
-  std::cout << "Hello " << name << " you are " << age1 << " years old!" << std::endl;
+  std::cout << "Hello " << full_name << " you are " << age << " years old!" << std::endl;
 }
