338301259dd6def8176c8331d671bea37648d0ad
Dharma <dharmaemmelene@gmail.com>
2022-02-27 13:43:54 +0000

    Braced inits

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -2,15 +2,25 @@
 #include <string>
 
 int main()
-// Number Systems
+// Integer Types
 {
-  int number1 = 15;         // Decimal
-  int number2 = 017;        // Octal
-  int number3 = 0x0F;       // Hexadecimal
-  int number4 = 0b00001111; // Binary
+  // Braced Initializers
+  int elephant_count; // Variable may contain random garbage value WARNING
+  std::cout << "Elephant count : " << elephant_count << std::endl;
 
-  std::cout << "number 1 : " << number1 << std::endl;
-  std::cout << "number 2 : " << number2 << std::endl;
-  std::cout << "number 3 : " << number3 << std::endl;
-  std::cout << "number 4 : " << number4 << std::endl;
+  int lion_count{}; // Inits to zero
+  std::cout << "Lion count : " << lion_count << std::endl;
+
+  int dog_count{10}; // Inits to 10
+  std::cout << "dog count : " << dog_count << std::endl;
+
+  int cat_count{15}; // Inits to 15
+  std::cout << "Cat count : " << cat_count << std::endl;
+
+  // Can use an expression as initializer
+  int domesticated_animals{dog_count + cat_count};
+  std::cout << "Domesticated animal count : " << domesticated_animals << std::endl;
+
+  // Functional Initialization
+  // int narrowing_conversion { 2.9 } // Compiler Error
 }
