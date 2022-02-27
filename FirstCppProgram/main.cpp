ba3afeb5b5392d0702a9a5915a96ca3532db78ea
Dharma <dharmaemmelene@gmail.com>
2022-02-27 13:56:38 +0000

    Assigment inits

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -22,7 +22,7 @@ int main()
     std::cout << "Domesticated animal count : " << domesticated_animals << std::endl; */
 
   // Functional Initialization
-  int apple_count(5);
+  /* int apple_count(5);
   int orange_count(10);
 
   int fruit_count(apple_count + orange_count);
@@ -33,5 +33,17 @@ int main()
   std::cout << "Apple count : " << apple_count << std::endl;
   std::cout << "Orange count : " << orange_count << std::endl;
   std::cout << "Fruit count : " << fruit_count << std::endl;
-  std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl;
+  std::cout << "Narrowing conversion : " << narrowing_conversion_functional << std::endl; */
+
+  // Assignment Initialization
+  int bike_count = 2;
+  int truck_count = 7;
+  int vehicle_count = bike_count + truck_count;
+
+  int narrowing_conversion_assignment = 2.9;
+
+  std::cout << "Bike count : " << bike_count << std::endl;
+  std::cout << "Truck count : " << truck_count << std::endl;
+  std::cout << "Vehicle count : " << vehicle_count << std::endl;
+  std::cout << "Narrowing conversion : " << narrowing_conversion_assignment << std::endl;
 }
