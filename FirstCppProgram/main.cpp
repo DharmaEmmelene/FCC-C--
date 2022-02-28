093e00177213192769188dc2304f10e926ea687c
Dharma <dharmaemmelene@gmail.com>
2022-02-28 13:33:31 +0000

    Scientific notation

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -23,4 +23,19 @@ int main()
   // Float issues: Precision is too limited for most applications
   float number4 = 192400023.0f; // Error: narrowing conversion
   std::cout << "number 4 is : " << number4 << std::endl;
+
+  std::cout << "------------------------------------------" << std::endl;
+
+  // Scientific notation
+  double number5{192400023};
+  double number6{1.92400023e8}; // multiply 10 exp(8)
+  double number7{1.924e8};      // omit the lower 00023 for simplicity
+  double number8{0.00000000003498};
+  double number9{3.498e-11}; // multiply 10 exp(-11)
+
+  std::cout << "number 5 is : " << number5 << std::endl;
+  std::cout << "number 6 is : " << number6 << std::endl;
+  std::cout << "number 7 is : " << number7 << std::endl;
+  std::cout << "number 8 is : " << number8 << std::endl;
+  std::cout << "number 9 is : " << number9 << std::endl;
 }
