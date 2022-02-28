4e64304f16fa0be689f70793efc649b5c852320d
Dharma <dharmaemmelene@gmail.com>
2022-02-28 14:38:35 +0000

    double assignment

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -8,4 +8,12 @@ int main()
 
   var1 = 55; // Assign
   std::cout << "var1 : " << var1 << std::endl;
+
+  std::cout << std::endl;
+
+  double var2{44.55}; // Declare & init
+  std::cout << "var2 : " << var2 << std::endl;
+
+  var2 = 99.99;
+  std::cout << "var2 : " << var2 << std::endl;
 }
