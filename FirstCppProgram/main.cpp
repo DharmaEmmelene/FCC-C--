09c3f29aef9196fff440a2c913e663e64eea0de7
Dharma <dharmaemmelene@gmail.com>
2022-03-08 12:44:36 +0000

    Store result in variable

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -13,10 +13,15 @@ int main()
 
   std::cout << std::boolalpha; // True/false instead of 1 / 0
 
-  std::cout << "number1 < number2 : " << (number1 < number2) << std::endl;
-  std::cout << "number1 <= number2 : " << (number1 <= number2) << std::endl;
-  std::cout << "number1 > number2 : " << (number1 > number2) << std::endl;
-  std::cout << "number1 >= number2 : " << (number1 >= number2) << std::endl;
-  std::cout << "number1 == number2 : " << (number1 == number2) << std::endl;
-  std::cout << "number1 != number2 : " << (number1 != number2) << std::endl;
+  std::cout << "number1 < number2 : " << (number1 < number2) << std::endl;   // true
+  std::cout << "number1 <= number2 : " << (number1 <= number2) << std::endl; // true
+  std::cout << "number1 > number2 : " << (number1 > number2) << std::endl;   // false
+  std::cout << "number1 >= number2 : " << (number1 >= number2) << std::endl; // false
+  std::cout << "number1 == number2 : " << (number1 == number2) << std::endl; // false
+  std::cout << "number1 != number2 : " << (number1 != number2) << std::endl; // true
+  std::cout << std::endl;
+
+  std::cout << "Store comparison result & use it later" << std::endl;
+  bool result = (number1 == number2);
+  std::cout << number1 << " == " << number2 << " : " << result << std::endl;
 }
