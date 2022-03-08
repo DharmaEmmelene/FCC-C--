6198ef516a3382cb3d2b5f3e66a6890fff9521ec
Dharma <dharmaemmelene@gmail.com>
2022-03-08 11:52:01 +0000

    Prefix Increment

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -17,12 +17,24 @@ int main()
   std::cout << "The value is : " << value << std::endl; // 4
   std::cout << "Postfix Increment & decrement" << std::endl;
 
-  value = 5;
+  value = 5;                                                             // Reset value
   std::cout << "The value is (incrementing) : " << value++ << std::endl; // 5
   std::cout << "The value is : " << value << std::endl;                  // 6
   std::cout << std::endl;
 
-  value = 5;
-  std::cout << "The value is (decrementing) : " << value-- << std::endl;
-  std::cout << "The value is : " << value << std::endl;
+  value = 5;                                                             // Reset value
+  std::cout << "The value is (decrementing) : " << value-- << std::endl; // 5
+  std::cout << "The value is : " << value << std::endl;                  // 4
+
+  std::cout << "Prefix Increment & decrement" << std::endl;
+
+  value = 5; // Reset value
+
+  ++value;
+  std::cout << "The value is (prefix++) : " << value << std::endl; // 6
+
+  value = 5;                                                                  // Reset value
+  std::cout << "The value is (prefix++ in place) : " << ++value << std::endl; // 6
+
+  std::cout << std::endl;
 }
