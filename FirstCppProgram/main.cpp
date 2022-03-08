2cffd9a5031f6f35397e02431d48fc917f744f6d
Dharma <dharmaemmelene@gmail.com>
2022-03-08 12:19:19 +0000

    > modulus

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -16,4 +16,6 @@ int main()
   std::cout << "The value is (after *=2) : " << value << std::endl; // 90
   value /= 3;
   std::cout << "The value is (after /=3) : " << value << std::endl; // 30
+  value %= 11;
+  std::cout << "The value is (after %=11) : " << value << std::endl; // 30 - (11*2) = 8
 }
