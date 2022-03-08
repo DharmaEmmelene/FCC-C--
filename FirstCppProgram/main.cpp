211fa85141b960ad006fd232e8b6fd8921118024
Dharma <dharmaemmelene@gmail.com>
2022-03-08 12:33:22 +0000

    Relational Ops "<"

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -2,20 +2,16 @@
 // Data Ops
 
 int main()
-// Compound Ops
+// Relational Ops
 {
-  int value{45};
-  std::cout << "The value is : " << value << std::endl;
-  std::cout << std::endl;
+  int number1{45};
+  int number2{60};
+  std::cout << "number1 : " << number1 << std::endl;
+  std::cout << "number2 : " << number2 << std::endl;
 
-  value += 5;                                                       // value = value + 5
-  std::cout << "The value is (after +=5) : " << value << std::endl; // 50
-  value -= 5;
-  std::cout << "The value is (after -=5) : " << value << std::endl; // 45
-  value *= 2;
-  std::cout << "The value is (after *=2) : " << value << std::endl; // 90
-  value /= 3;
-  std::cout << "The value is (after /=3) : " << value << std::endl; // 30
-  value %= 11;
-  std::cout << "The value is (after %=11) : " << value << std::endl; // 30 - (11*2) = 8
+  std::cout << "-----------Comparing variables------------" << std::endl;
+
+  std::cout << std::boolalpha; // True/false instead of 1 / 0
+
+  std::cout << "number1 < number2 : " << (number1 < number2) << std::endl;
 }
