b68bdbd86074d76e6e34fe95f67904da3b96ce2f
Dharma <dharmaemmelene@gmail.com>
2022-03-14 13:38:47 +0000

    Format table with int

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -69,4 +69,13 @@ int main()
 
   std::cout
       << "------------" << std::endl;
+
+  int col_width{14};
+  std::cout << std::setw(col_width) << "Lastname" << std::setw(col_width) << "Firstname" << std::setw(col_width) << "Age" << std::endl;
+  std::cout << std::setw(col_width) << "Gray" << std::setw(col_width) << "Daniel" << std::setw(col_width) << "25" << std::endl;
+  std::cout << std::setw(col_width) << "Woods" << std::setw(col_width) << "Stanley" << std::setw(col_width) << "33" << std::endl;
+  std::cout << std::setw(col_width) << "Parker" << std::setw(col_width) << "Jordan" << std::setw(col_width) << "45" << std::endl;
+  std::cout << std::setw(col_width) << "Ball" << std::setw(col_width) << "Joe" << std::setw(col_width) << "21" << std::endl;
+  std::cout << std::setw(col_width) << "Carr" << std::setw(col_width) << "Josh" << std::setw(col_width) << "27" << std::endl;
+  std::cout << std::setw(col_width) << "Robinson" << std::setw(col_width) << "Izaiah" << std::setw(col_width) << "29" << std::endl;
 }
