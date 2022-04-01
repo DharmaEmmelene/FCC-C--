98fd20393402d577d4092d14d322800f6ec1e2d1
Dharma <dharmaemmelene@gmail.com>
2022-04-01 11:36:29 +0100

    Using log()

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -9,26 +9,31 @@ int main()
 
   //floor
   std::cout << "Weight rounded to floor is : " << std::floor(weight) << std::endl;
-  std::cout << "---------------------------------------------" <<std::endl;
 
   //ceil
   std::cout << "Weight rounded to ceil is : " << std::ceil(weight) << std::endl;
-  std::cout << "---------------------------------------------" <<std::endl;
-
+ 
   //abs
   double savings {-5000};
 
   std::cout << "Abs of weight is : " << std::abs(weight) << std::endl;
   std::cout << "Abd of savings is : " << std::abs(savings) << std::endl;
-  std::cout << "---------------------------------------------" <<std::endl;
 
   //exp : f(x) = e ^ x , where e = 2.71828 
   double exponential = std::exp(10);
   std::cout << "The exponential of 10 is : " << exponential << std::endl;
-  std::cout << "---------------------------------------------" <<std::endl;
 
   // pow
   std::cout << "3 ^ 4 is : " << std::pow(3, 4) <<std::endl;
   std::cout <<" 9 ^ 3 is : " << std::pow(9, 3) <<std::endl;
   std::cout << "---------------------------------------------" <<std::endl;
+
+  //log : reverse function of pow. if 2 ^ 3 = 8, log 8 in base 2 = 3
+  //in the above example log asks to which exponent should we elevate 2 to get 8?
+  //log by default computes in base e
+
+  //reverse op of e ^ 4 = 54.59
+  std::cout << "Log ; to get 54.59, you would elevate e to the power of : " << std::log(54.59) << std::endl;
+  //log(10), 10 ^ 4 = 10000, elevate 10 to the power of
+  std::cout << "To get 1000, elevate 10 to the power of : " << std::log10(10000) << std::endl;
 }
