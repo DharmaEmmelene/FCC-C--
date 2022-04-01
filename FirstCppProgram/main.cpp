b954792a23323f314516c570d8c4f637674d27e0
Dharma <dharmaemmelene@gmail.com>
2022-04-01 13:30:25 +0100

    Integral types

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -1,47 +1,24 @@
 #include <iostream>
-#include <cmath>
+
 // Data Ops
 
 int main()
-// Math functions
+// Weird Integral Types
 {
-  /* double weight {7.7};
+  short int var1 {10};
+  short int var2 {20};
 
-  //floor
-  std::cout << "Weight rounded to floor is : " << std::floor(weight) << std::endl;
+  char var3 {40};
+  char var4 {50};
 
-  //ceil
-  std::cout << "Weight rounded to ceil is : " << std::ceil(weight) << std::endl;
- 
-  //abs
-  double savings {-5000};
+  std::cout << "size of var1 : " << sizeof(var1) << std::endl;
+  std::cout << "size of var2 : " << sizeof(var2) << std::endl;
+  std::cout << "size of var3 : " << sizeof(var3) << std::endl;
+  std::cout << "size of var4 : " << sizeof(var4) << std::endl;
 
-  std::cout << "Abs of weight is : " << std::abs(weight) << std::endl;
-  std::cout << "Abd of savings is : " << std::abs(savings) << std::endl;
+  auto result1 = var1 + var2;
+  auto result2 = var3 + var4;
 
-  //exp : f(x) = e ^ x , where e = 2.71828 
-  double exponential = std::exp(10);
-  std::cout << "The exponential of 10 is : " << exponential << std::endl;
-
-  // pow
-  std::cout << "3 ^ 4 is : " << std::pow(3, 4) <<std::endl;
-  std::cout <<" 9 ^ 3 is : " << std::pow(9, 3) <<std::endl;
-  std::cout << "---------------------------------------------" <<std::endl;
-
-  //log : reverse function of pow. if 2 ^ 3 = 8, log 8 in base 2 = 3
-  //in the above example log asks to which exponent should we elevate 2 to get 8?
-  //log by default computes in base e
-
-  //reverse op of e ^ 4 = 54.59
-  std::cout << "Log ; to get 54.59, you would elevate e to the power of : " << std::log(54.59) << std::endl;
-  //log(10), 10 ^ 4 = 10000, elevate 10 to the power of
-  std::cout << "To get 1000, elevate 10 to the power of : " << std::log10(10000) << std::endl; */
-
-  //sqrt
-  std::cout << "The square root of 81 is : " << std::sqrt(81) << std::endl;
-
-  //round Halfway point are rounded away from 0
-  std::cout << "3.654 rounded to : " << std::round(3.654) << std::endl;
-  std::cout << "2.5 rounded to : " << std::round(2.5) << std::endl;
-  std::cout << "2.4 rounded to : " << std::round(2.4) << std::endl;
+  std::cout << "size of result1 : " << sizeof(result1) << std::endl;
+  std::cout << "size of result2 : " << sizeof(result2) << std::endl;
 }
